a157412481beddfad2cbebb1942842fdf03abc60
Erik P <erikjensenpratt+git@gmail.com>
2018-01-11 15:49:28 -0800

    Example command doesn't require example subsystem

--- src/org/usfirst/frc/team1983/robot/commands/ExampleCommand.java
+++ src/org/usfirst/frc/team1983/robot/commands/ExampleCommand.java
@@ -9,8 +9,6 @@ import org.usfirst.frc.team1983.robot.Robot;
  */
 public class ExampleCommand extends Command {
 	public ExampleCommand() {
-		// Use requires() here to declare subsystem dependencies
-		requires(Robot.exampleSubsystem);
 	}
 
 	// Called just before this Command runs the first time
