6c4a51a07399c83cc524dd60042d80d2875eb387
linknmike <mikeheidal@gmail.com>
2018-01-11 19:19:21 -0800

    created a stub of turnAngle!
    still needs pid values to be created. that's definitely something. maybe
    placeholder values?

---
+++ src/org/usfirst/frc/team1983/robot/commands/TurnAngle.java
@@ -0,0 +1,57 @@
+package org.usfirst.frc.team1983.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1983.robot.subsystems.utilities.PidControlWrapper;
+
+public class TurnAngle extends Command
+{
+
+    double initialAngle;
+    double targetAngle;
+    PidControlWrapper turnPid;
+
+    //the constructor takes a number of degrees it wants to turn. Positive is clockwise, negative is counterclockwise.
+    public TurnAngle(double degrees)
+    {
+        //requires drivebase! that's not implemented yet and not quite in the scope of this commit.
+        targetAngle = degrees;
+    }
+
+    @Override
+    protected void initialize()
+    {
+
+        initialAngle = 0;
+        //TODO: get p i d values here
+        // turnPid = new PidControlWrapper();
+        turnPid.setSetpoint(targetAngle);
+
+    }
+
+    @Override
+    protected void execute()
+    {
+
+    }
+
+    @Override
+    protected boolean isFinished()
+    {
+        return turnPid.onTarget();
+    }
+
+    @Override
+    protected void end()
+    {
+
+        turnPid.disable();
+
+    }
+
+
+    @Override
+    protected void interrupted()
+    {
+        super.interrupted();
+    }
+}
