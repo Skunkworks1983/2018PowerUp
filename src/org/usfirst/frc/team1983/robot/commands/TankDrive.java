829f91e26193de66493445881402d2a1b043d6a7
Unknown <benevedeselias@gmail.com>
2018-01-13 15:12:25 -0800

    Fixed compilation errors (whoops)

--- src/org/usfirst/frc/team1983/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team1983/robot/commands/TankDrive.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team1983.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc.team1983.robot.Robot;
+import frc.team1983.Robot;
 
 
 public class TankDrive extends Command
