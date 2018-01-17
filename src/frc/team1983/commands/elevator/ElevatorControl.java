b56a4ba1679662eb8ba614cdf40b2fa669beff70
Erik P <erikjensenpratt+git@gmail.com>
2018-01-16 20:47:22 -0800

    Added control methods

--- src/frc/team1983/commands/elevator/ElevatorControl.java
+++ src/frc/team1983/commands/elevator/ElevatorControl.java
@@ -1,10 +1,13 @@
 package frc.team1983.commands.elevator;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.team1983.OI;
 import frc.team1983.Robot;
+import frc.team1983.settings.Misc;
 import frc.team1983.settings.PidValues;
 import frc.team1983.subsystems.Elevator;
 import frc.team1983.subsystems.utilities.inputwrappers.ElevatorPidInput;
@@ -17,23 +20,49 @@ public class ElevatorControl extends Command
     private PIDController controller;
     private PIDSource pidIn;
     private PIDOutput pidOut;
+    private OI oi;
 
     public ElevatorControl()
     {
-        requires(Robot.getInstance().getElevator());
         elevator = Robot.getInstance().getElevator();
+        requires(elevator);
 
         pidIn = new ElevatorPidInput();
         pidOut = new ElevatorControlPidOutput(elevator);
         controller = new PIDController(PidValues.ELEVATOR_CONTROL_P, PidValues.ELEVATOR_CONTROL_I, PidValues.ELEVATOR_CONTROL_D,
-                PidValues.ELEVATOR_CONTROL_F, pidIn, pidOut);
+                                       PidValues.ELEVATOR_CONTROL_F, pidIn, pidOut);
+        controller.setSetpoint(0);
+        oi = Robot.getInstance().getOI();
+    }
+
+    public void setPIDSetpoint(double setPoint)
+    {
+        controller.setSetpoint((int) setPoint * Misc.ELEVATOR_ENCODER_SCALAR);
+    }
+
+    public int getPIDSetpoint()
+    {
+        return (int) controller.getSetpoint();
     }
 
     @Override
-    protected void initialize() {}
+    protected void initialize()
+    {
+        controller.enable();
+    }
 
     @Override
-    protected void execute() {}
+    protected void execute()
+    {
+        if(oi.buttons.isDown(0))
+        {
+            setPIDSetpoint(oi.getSliderPos());
+        }
+        else
+        {
+            
+        }
+    }
 
     @Override
     protected boolean isFinished()
@@ -42,10 +71,14 @@ public class ElevatorControl extends Command
     }
 
     @Override
-    protected void end() {}
+    protected void end()
+    {
+        controller.free();
+        //todo make a fuss in the logger, because this command should always have control over the elevator
+    }
 
     @Override
-    protected void interrupted ()
+    protected void interrupted()
     {
         this.end();
     }
--- src/frc/team1983/settings/Misc.java
+++ src/frc/team1983/settings/Misc.java
@@ -2,8 +2,15 @@ package frc.team1983.settings;
 
 public class Misc
 {
-    public static double JOYSTICK_TOLERANCE = 0.5;
+    public static final double JOYSTICK_TOLERANCE = 0.5;
 
     //Scalar coefficient of the slider on the OI
-    public static double SLIDER_SCALAR = 0.618726;
+    public static final double SLIDER_SCALAR = 0.618726;
+
+    /*
+    We give the elevator a double between 0 and 1, but the encoder operates
+    on a currently unknown scale from 0 to whatever, so multiply the the input
+    by this value to have 1 be the maximum
+    */
+    public static final int ELEVATOR_ENCODER_SCALAR = 1; //TODO get the real max value
 }
