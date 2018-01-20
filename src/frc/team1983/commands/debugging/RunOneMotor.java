52cb74359004fe01004d638ff2a0bc727fb6bda5
Naba Deyab <nabadeyab@gmail.com>
2018-01-20 10:34:46 -0800

    Merge branch 'dev' into RunOneMotor
    # Conflicts:
    #	src/frc/team1983/OI.java
    #	src/frc/team1983/subsystems/Drivebase.java
    #	src/frc/team1983/subsystems/sensors/Gyro.java

--- src/frc/team1983/commands/debugging/RunOneMotor.java
+++ src/frc/team1983/commands/debugging/RunOneMotor.java
@@ -2,15 +2,18 @@ package frc.team1983.commands.debugging;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team1983.Robot;
+import frc.team1983.services.OI;
 import frc.team1983.subsystems.utilities.Motor;
 
 public class RunOneMotor extends Command {
    private boolean continuousPress = false;
    private int motorIndex = 0;
+   private OI oi;
 
    private Motor motors[]= new Motor[16];
 
-   public RunOneMotor(){
+   public RunOneMotor(OI oi){
+       this.oi = oi;
        for(int i = 0; i<16; i++){
            motors[i] = new Motor (i, NeutralMode.Coast,  false);
 //           Motor m = new Motor (i, NeutralMode.Coast, false);
@@ -25,7 +28,7 @@ public class RunOneMotor extends Command {
    protected void execute(){
        //motors[i].set(???){
         if(!continuousPress){
-            if(Robot.getInstance().getOI().getLeftTriggerPressed()){
+            if(oi.isPressed(0,0)){ //triggers are button 0
                 if(motorIndex == 0){
                     motorIndex = 15;
                 }
@@ -34,7 +37,7 @@ public class RunOneMotor extends Command {
                 }
                 continuousPress = true;
        }
-       else if (Robot.getInstance().getOI().getRightTriggerPressed()){
+       else if (oi.isPressed(1,0)){
                 if (motorIndex == 15){
                     motorIndex = 0;
                 }
@@ -47,10 +50,11 @@ public class RunOneMotor extends Command {
    else {
             continuousPress = false;
         }
-        if (Robot.getInstance().getOI().getButtonPushed(2)){
+        if (oi.isDown(2,2)){
             System.out.println(motors[motorIndex].getSelectedSensorPosition(0));
         }
-   double joy = Robot.getInstance().getOI().getLeftStickY();
+   double joy = oi.getAxis(0, 1)
+           //Robot.getInstance().getOI().getLeftStickY();
    if (joy > - 0.05 && joy < 0.05) {
        joy = 0;
    }
