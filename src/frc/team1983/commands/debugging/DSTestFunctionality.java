86abb771a432051c9ff035789c1e390727c25403
Naba Deyab <nabadeyab@gmail.com>
2018-01-27 14:34:57 -0800

    button integration

--- src/frc/team1983/commands/debugging/DSTestFunctionality.java
+++ src/frc/team1983/commands/debugging/DSTestFunctionality.java
@@ -1,6 +1,7 @@
 package frc.team1983.commands.debugging;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.team1983.subsystems.utilities.Motor;
 
 public class DSTestFunctionality
@@ -9,6 +10,7 @@ public class DSTestFunctionality
     private Motor motors[] = new Motor [1];
     // there is only one space in the motor array, since we are only running one motor at a time
     // on 2017, motor 6 is the collector belt
+    private DigitalInput motorSelector = new DigitalInput(0);
 
     public DSTestFunctionality()
     {
@@ -27,6 +29,14 @@ public class DSTestFunctionality
     {
         // this sets the speed of the selected motor to half speed automatically
         motors[motorIndex].set(0.5);
+        if (motorSelector.get())
+        {
+            System.out.println("motorSelector returns true");
+        }
+        else
+        {
+            System.out.println("motorSelector returns false");
+        }
     }
     public void end()
     {
