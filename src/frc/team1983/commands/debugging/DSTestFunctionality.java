afe215ddbbec53dd029da0649c2cc8d77d486998
Naba Deyab <nabadeyab@gmail.com>
2018-02-02 17:23:12 -0800

    updates to include speed

--- src/frc/team1983/commands/debugging/DSTestFunctionality.java
+++ src/frc/team1983/commands/debugging/DSTestFunctionality.java
@@ -1,6 +1,7 @@
 package frc.team1983.commands.debugging;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team1983.subsystems.utilities.Motor;
@@ -12,7 +13,7 @@ public class DSTestFunctionality
     // there is only one space in the motor array, since we are only running one motor at a time
     // on 2017, motor 6 is the collector belt
     private DigitalInput motorSelector;
-    private AnalogInnput motorSpeed;
+    private AnalogInput manualSpeed;
 
     public DSTestFunctionality()
     {
@@ -22,27 +23,32 @@ public class DSTestFunctionality
             motors[motorIndex] = new Motor (port, NeutralMode.Brake,true);
             // the motor indexed here is given the constraints required
             motorSelector = new DigitalInput(5);
+            manualSpeed = new AnalogInput(2);
         }
     }
-    public void initialize()
+    protected void initialize()
     {
       //nothing
         //motors[motorIndex].set(0.25);
     }
-    public void execute()
+    protected void execute()
     {
         // this sets the speed of the selected motor to half speed automatically
        // motors[motorIndex].set(0.5);
-        if (motorSelector != null)
+       /* if (motorSelector != null)
         {
             if (motorSelector.get()) {
                 motors[motorIndex].set(0);
             } else {
                 motors[motorIndex].set(0.5);
             }
+        } */
+        if (manualSpeed != null)
+        {
+            motors[motorIndex].set((manualSpeed.getVoltage()/2.5)-1);
         }
     }
-    public void end()
+    protected void end()
     {
         // if this were called within the test, then the motor would be turned off
         // since this is currently not called, in order to shut off the motor you need to disable it
