b615a4efe286e4e23c529af5d0f82246a8ab1a88
Naba Deyab <nabadeyab@gmail.com>
2018-02-12 18:18:10 -0800

    updates ?

--- test/frc/team1983/commands/debugging/UT_DSTestFunctionality.java
+++ test/frc/team1983/commands/debugging/UT_DSTestFunctionality.java
@@ -1,5 +1,30 @@
 package frc.team1983.commands.debugging;
 
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.hal.HAL;
+import frc.team1983.subsystems.utilities.Motor;
+import org.hamcrest.Matchers;
+import org.hamcrest.number.IsCloseTo;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class UT_DSTestFunctionality
 {
+    static
+    {
+        HAL.initialize(500,0);
+    }
+
+    private DSTestFunctionality testFunct;
+
+
 }
