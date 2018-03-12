92a49b6861ad20f0170abc569877cf7faf12531f
linknmike <mikeheidal@gmail.com>
2018-03-11 18:31:45 -0700

    refactored ya bois

--- src/frc/team1983/commands/autonomous/deadreckoningautos/DoubleCubeAutoSelector.java
+++ src/frc/team1983/commands/autonomous/deadreckoningautos/DoubleCubeAutoSelector.java
@@ -1,4 +1,4 @@
-package frc.team1983.commands.autonomous.doublecubeautos;
+package frc.team1983.commands.autonomous.deadreckoningautos;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
