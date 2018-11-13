a58675ebb06100ec0416361174ce96cb767c8724
-v <nathantresham@gmail.com>
2018-11-13 13:18:36 -0800

    deleted unused files

--- src/frc/team1983/subsystems/utilities/inputwrappers/DriveStraightPidInput.java
+++
@@ -1,5 +0,0 @@
-package frc.team1983.subsystems.utilities.inputwrappers;
-
-public class DriveStraightPidInput
-{
-}
--- src/frc/team1983/utility/motion/profiles/SinusoidalProfile.java
+++
@@ -1,5 +0,0 @@
-package frc.team1983.utility.motion.profiles;
-
-public class SinusoidalProfile
-{
-}
